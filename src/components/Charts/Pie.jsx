205dbfca6c7c26819f0fd4699191d4768ecd88e7
Ranjith <ranjith07509@gmail.com>
2025-04-04 16:38:22 +0530

    final dashboard

---
+++ src/components/Charts/Pie.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Pie extends Component {
+  render() {
+    return (
+        <>
+         <div>Pie</div>
+         <div>Data</div>
+        </>
+     
+    )
+  }
+}
+
+export default Pie
